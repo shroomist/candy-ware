05f835659873c1a36ec2a6edc15b9fecf7363eee
Andrej Novikov <jazzaiman@gmail.com>
2021-06-01 17:10:27 +0300

    feat: instr state

---
+++ src/state.cpp
@@ -0,0 +1,4 @@
+#include "state.hpp"
+
+byte currentChannel = 0;
+byte channelInstr[9] = {0,1,3,4,5,6,7,8};
---
+++ src/state.hpp
@@ -0,0 +1,10 @@
+#ifndef STATE_H_
+#define STATE_H_
+
+#include <Arduino.h>
+
+extern byte currentChannel;
+
+extern byte channelInstr[9];
+
+#endif // STATE_H_
